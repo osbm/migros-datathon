edfa2d9260f4573f77e250e8c821565b4dd6f136
osbm <osmanfbayram@gmail.com>
2022-12-08 20:11:34 +0300

    fix the command sections

--- .github/scripts/get_kaggle_score.py
+++ .github/scripts/get_kaggle_score.py
@@ -9,11 +9,13 @@ competition_name = sys.argv[1]
 
 # get the score from the kaggle leaderboard
 def get_kaggle_score():
-    result = subprocess.Popen(
-        f"kaggle competitions leaderboard -c {competition_name} --csv",
-        shell=True,
-    )
-    df = pd.read_csv(result.stdout)
+    command = f"kaggle competitions submissions -c {competition_name} --csv"
+    result = subprocess.Popen(command,shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
+    result = result.decode("utf-8") # convert bytes to string
+    with open("result.csv", "w") as f:
+        f.write(result)
+
+    df = pd.read_csv("result.csv")
     return df["publicScore"].iloc[0]
 
 def save_env_variable(var_name, var_value):
@@ -26,5 +28,6 @@ def save_env_variable(var_name, var_value):
 if __name__ == "__main__":
     time.sleep(60) # wait for the submission to be processed
     score = get_kaggle_score()
+
     print (f"Kaggle score: {score}")
     save_env_variable("KAGGLE_SCORE", str(score))
